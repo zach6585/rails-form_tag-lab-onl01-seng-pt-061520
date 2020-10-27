75cb5c2d0fca55b36fe90d49f08b73bdbf93f659
zach6585 <zmines@wesleyan.edu>
2020-10-27 00:11:54 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   
   def index
@@ -5,7 +6,15 @@ class StudentsController < ApplicationController
   end
 
   def show
+    # binding.pry
     @student = Student.find(params[:id])
   end
 
+  def new 
+  end 
+
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "jebecky", last_name: "James")
